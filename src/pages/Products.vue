eae296bd0b8273473a19c2ffc6d1e3222bc85b13
Vladimir Petrovic <petrovic.vladimir92@gmail.com>
2018-04-27 20:38:37 +0200

    Search products

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <table class="table mt-4 ml-1">
+    <input type="text" class="mt-2 ml-1" v-model="search" placeholder="Search" />
+    <table class="table mt-1 ml-1">
       <thead class="thead-light">
         <tr>
           <th scope="col">ID</th>
@@ -9,7 +10,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="product in products" :key="product.id">
+        <tr v-for="product in filteredProducts" :key="product.id">
           <td>{{ product.id }}</td>
           <td>{{ product.title }}</td>
           <td>{{ product.quantity }}</td>
@@ -24,11 +25,19 @@ import { productService } from '../utils/ProductService.js'
 export default {
   data() {
     return {
+      search: '',
       products: []
     }
   },
   created() {
     this.products = productService.getProducts();
+  },
+  computed: {
+    filteredProducts() {
+      return this.products.filter(product => {
+        return product.title.toLowerCase().includes(this.search.toLowerCase())
+      })
+    }
   }
 }
 </script>
