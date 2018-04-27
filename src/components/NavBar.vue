1ae32274e1c455fcd11fe9a35740627db8a356a0
Vladimir Petrovic <petrovic.vladimir92@gmail.com>
2018-04-27 18:26:30 +0200

    added bootstrap, initial components and links

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <router-link class="navbar-brand" to="/">My Store</router-link>
+        <div class="navbar-nav">
+          <router-link class="nav-item nav-link" to="customers">Customers</router-link>
+          <router-link class="nav-item nav-link" to="products">Products</router-link>
+        </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Customers from './pages/Customers.vue'
+import Products from './pages/Products.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', redirect: '/customers' },
+  { path: '/customers', component: Customers },
+  { path: '/products', component: Products }
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
