438c22e3a3b3adaefaf6568c5c48c01d0b484f46
Vladimir Petrovic <petrovic.vladimir92@gmail.com>
2018-04-27 19:26:02 +0200

    Add new customer function

--- src/pages/Customers.vue
+++ src/pages/Customers.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <table class="table mt-4">
+    <table class="table mt-4 ml-1">
       <thead class="thead-light">
         <tr>
           <th scope="col">ID</th>
@@ -11,7 +11,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(customer, key) in customers" :key="key">
+        <tr v-for="(customer, key) in customers" :key="customer.id">
           <td>{{ customer.id }}</td>
           <td>{{ customer.firstName }}</td>
           <td>{{ customer.lastName }}</td>
@@ -22,25 +22,49 @@
         </tr>
       </tbody>
     </table>
+
+    <form @submit.prevent="addCustomer">
+      <div class="form-group col-4">
+        <label for="firstName">First Name</label>
+        <input type="text" v-model="newCustomer.firstName" class="form-control" id="firstName">
+      </div>
+      <div class="form-group col-4">
+        <label for="lastName">Last Name</label>
+        <input type="text" v-model="newCustomer.lastName" class="form-control" id="lastName">
+      </div>
+      <div class="form-group col-4">
+        <label for="email">Email address</label>
+        <input type="email" v-model="newCustomer.email" class="form-control" id="email">
+      </div>
+      <button class="btn btn-primary btn-sm ml-3 mb-2">Add Customer</button>
+    </form>
   </div>
 </template>
 
 <script>
-import CustomerService from '../utils/CustomerService.js'
-const customerService = new CustomerService()
+import { customerService } from '../utils/CustomerService.js'
 
   export default {
     data() {
       return {
-        customers: []
+        customers: [],
+        newCustomer: {
+          firstName: '',
+          lastName: '',
+          email: ''
+        }
       }
     },
     created() {
-      this.customers = customerService.customers()
+      this.customers = customerService.getCustomers()
     },
     methods: {
-      deleteCustomer(key) {
-        this.customers.splice(key, 1)
+      deleteCustomer(id) {
+        customerService.deleteCustomer(id)
+      },
+      addCustomer() {
+        customerService.addCustomer(this.newCustomer)
+        this.newCustomer = {}
       }
     }
   }
--- src/utils/CustomerService.js
+++ src/utils/CustomerService.js
@@ -1,13 +1,24 @@
+const customers = [
+  { id: 1, firstName: 'Pera', lastName: 'Peric', email: 'pera@peric.com' },
+  { id: 2, firstName: 'Joca', lastName: 'Jocic', email: 'joca@jocic.com' },
+  { id: 3, firstName: 'Mika', lastName: 'Brca', email: 'mika@brca.com' },
+  { id: 4, firstName: 'Test', lastName: 'Testic', email: 'test@testic.com' },
+  { id: 5, firstName: 'Jova', lastName: 'Jovic', email: 'jova@jovic.com' }
+]
+
 export default class CustomerService {
-    customers() {
-        return [
-            { id: 1, firstName: 'Pera', lastName: 'Peric', email: 'pera@peric.com' },
-            { id: 2, firstName: 'Joca', lastName: 'Jocic', email: 'joca@jocic.com' },
-            { id: 3, firstName: 'Mika', lastName: 'Brca', email: 'mika@brca.com' },
-            { id: 4, firstName: 'Test', lastName: 'Testic', email: 'test@testic.com' },
-            { id: 5, firstName: 'Jova', lastName: 'Jovic', email: 'jova@jovic.com' },
-        ]
-    }
+  getCustomers() {
+    return customers;
+  }
+
+  deleteCustomer(id) {
+    customers.splice(id, 1)
+  }
+
+  addCustomer(newCustomer) {
+    newCustomer.id = customers.length + 1
+    customers.push(newCustomer)
+  }
 }
 
 export const customerService = new CustomerService()
